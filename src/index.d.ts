362b6edb47b6c4e6c1dd5bec76e213f39880f717
ntonjsgo <asgobio.work@gmail.com>
2024-07-19 12:41:32 +0200

    fix

--- src/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare module "@medusajs/medusa/dist/models/cart" {
-  declare interface Cart {
-    abandoned_completed_at?: Date;
-    abandoned_count?: number;
-    abandoned_last_interval?: number;
-    abandoned_lastdate?: Date;
-  }
-}
